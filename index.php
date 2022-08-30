bf09f3c15f368a97d44d8828964342b789d6d395
vayamos-exchange <admin@vayamos.cc>
2022-08-30 14:17:31 +0200

    2 cols from wallet/overview

--- index.php
+++ index.php
@@ -14,37 +14,72 @@
         <?php include('../../templates/navbar.php'); ?>
         
         <!-- Root container -->
-        <div id="root" class="container-fluid container-1500 p-0 user-only">
+        <div id="root" class="container-fluid container-1500 p-0">
         <div class="row m-0 h-rest">
         
         <!-- Main column -->
-        <div class="col-12 p-0 ui-card ui-column">
-            <div class="row p-2">
-                <h3>Transactions history</h3>
+        <div class="col-12 col-lg-9 p-0 ui-card ui-column">
+            
+            <form>
+            <div class="row p-2 flex-nowrap">
+                <div class="col-auto my-auto px-1">
+                    <a href="/wallet/deposit" class="btn btn-primary btn-sm">Deposit</a>
+                    <a href="/wallet/withdraw" class="btn btn-primary btn-sm">Withdraw</a>
+                    <a href="/wallet/transfer" class="btn btn-primary btn-sm">Transfer</a>
+                </div>
+                
+                <div class="col-auto my-auto px-1">
+                    <input id="asset-search" type="text" size="7" placeholder="Search" class="form-control input-search">
+                </div>
+                
+                <div class="col-auto my-auto px-1">
+                    <div class="form-check">
+                        <input class="form-check-input" type="checkbox" value="" id="asset-hide-zero">
+                        <label class="form-check-label" for="asset-hide-zero">
+                            Hide zero
+                        </label>
+                    </div>
+                </div>
             </div>
+            </form>
             
             <div class="row p-2 secondary d-none d-lg-flex">
-                <div style="width: 20%">
-                    <h5>Date</h5>
+                <div style="width: 60px">
                 </div>
-                <div style="width: 20%">
-                    <h5>Type</h5>
+                <div style="width: calc(20% - 60px)">
+                <h5>Token</h5>
                 </div>
-                <div style="width: 20%">
-                    <h5>Asset</h5>
+                <div class="text-end" style="width: 19%">
+                <h5>Total</h5>
                 </div>
-                <div class="text-end" style="width: 20%">
-                    <h5>Amount</h5>
+                <div class="text-end" style="width: 19%">
+                <h5>Available</h5>
                 </div>
-                <div class="text-end" style="width: 20%">
-                    <h5>Status</h5>
+                <div class="text-end" style="width: 18%">
+                <h5>Locked</h5>
+                </div>
+                <div style="width: 24%">
+                <h5>Action</h5>
                 </div>
             </div>
             
-            <div id="transactions-data">
+            <div id="asset-data">
             </div>
         
         <!-- / Main column -->
+        </div>
+        
+        <!-- Right column -->
+        <div class="col-12 col-lg-3 p-0 ui-card ui-column d-none d-lg-block">
+        
+            <div class="row p-2">
+                <h3>Recent P2P trades</h3>
+            </div>
+            
+            <div id="recent-tx-data">
+            </div>
+        
+        <!-- / Right column -->
         </div>
             
         <!-- / Root container -->    
