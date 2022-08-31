b2191593bf710718d0c0b22acbea223896f33f2d
vayamos-exchange <admin@vayamos.cc>
2022-08-31 15:21:17 +0200

    Svg test

--- js/select_fiat.js
+++ js/select_fiat.js
@@ -54,7 +54,7 @@ function initSelectFiat(endpoint = '/p2p/fiats') {
                                 <div class="col-auto my-auto text-center" style="width: 32px">
                                     <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg">
                                         <circle cx="50%" cy="50%" r="50%" fill="white" />
-                                        <text x="50%" y="50%" text-anchor="middle">${v.symbol}</text>
+                                        <text x="50%" y="50%" lengthAdjust="spacingAndGlyphs">${v.symbol}</text>
                                     </svg>
                                 </div>
                                 <div class="col my-auto">
