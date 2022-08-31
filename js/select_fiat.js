8e80f7ad8c5157b8d110e567299221826c19401a
vayamos-exchange <admin@vayamos.cc>
2022-08-31 19:15:13 +0200

    Align Center

--- js/select_fiat.js
+++ js/select_fiat.js
@@ -52,7 +52,7 @@ function initSelectFiat(endpoint = '/p2p/fiats') {
                         thisAS.append(`
                             <div class="select-fiat-item row p-1 hoverable" data-asset="${k}">
                                 <div class="col-auto my-auto">
-                                    <div class="bg-white text-center align-middle rounded-circle" style="width: 24px; height: 24px; color: black;">
+                                    <div class="bg-white align-items-center rounded-circle" style="width: 24px; height: 24px; color: black;">
                                         ${v.symbol}
                                     </div>
                                 </div>
