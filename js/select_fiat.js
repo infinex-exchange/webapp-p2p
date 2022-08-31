061e0c54c26e7e9bb311ffa95e0cfa568bbbaf7d
vayamos-exchange <admin@vayamos.cc>
2022-08-31 15:13:34 +0200

    Svg test

--- js/select_fiat.js
+++ js/select_fiat.js
@@ -52,8 +52,7 @@ function initSelectFiat(endpoint = '/p2p/fiats') {
                         thisAS.append(`
                             <div class="select-fiat-item row p-1 hoverable" data-asset="${k}">
                                 <div class="col-auto my-auto text-center" style="width: 32px">
-                                    <svg viewBox="0 0 10 10" xmlns="http://www.w3.org/2000/svg">
-                                        <rect x="0" y="0" width="100%" height="100%"/>
+                                    <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                                         <circle cx="50%" cy="50%" r="4" fill="white"/>
                                     </svg>
                                 </div>
