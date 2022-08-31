e6638042b3bc73d01b486eb9a0e852ad10274bf7
vayamos-exchange <admin@vayamos.cc>
2022-08-31 19:09:48 +0200

    Div with white bg

--- js/select_fiat.js
+++ js/select_fiat.js
@@ -51,8 +51,10 @@ function initSelectFiat(endpoint = '/p2p/fiats') {
                     $.each(data.fiats, function(k, v) {
                         thisAS.append(`
                             <div class="select-fiat-item row p-1 hoverable" data-asset="${k}">
-                                <div class="col-auto my-auto text-center bg-white" style="width: 32px">
-                                    ${v.symbol}
+                                <div class="col-auto my-auto text-center" style="width: 32px">
+                                    <div class="bg-white" style="width: 24px; height: 24px;">
+                                        ${v.symbol}
+                                    </div>
                                 </div>
                                 <div class="col my-auto">
                                     <strong>${k}</strong>
