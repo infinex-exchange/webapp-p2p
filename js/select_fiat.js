cf165506694ce6ea82a1909db2fe797d469c46ad
vayamos-exchange <admin@vayamos.cc>
2022-08-31 23:19:26 +0200

    Span -> strong

--- js/select_fiat.js
+++ js/select_fiat.js
@@ -53,7 +53,7 @@ function initSelectFiat(endpoint = '/p2p/fiats') {
                             <div class="select-fiat-item row p-1 hoverable" data-asset="${k}">
                                 <div class="col-auto my-auto">
                                     <div class="bg-white text-center rounded-circle" style="width: 24px; height: 24px; color: black;">
-                                        <strong class="align-middle">${v.symbol}</span>
+                                        <strong class="align-middle">${v.symbol}</strong>
                                     </div>
                                 </div>
                                 <div class="col my-auto">
