b901b050ca81b19342fef5621d304ce8b4868638
vayamos-exchange <admin@vayamos.cc>
2022-09-01 23:33:21 +0200

    Add config ajax

--- js/index.js
+++ js/index.js
@@ -24,4 +24,26 @@ $(document).ready(function() {
         window.p2pOffersAs.data.fpm = $(this).data('fpmid');
         window.p2pOffersAs.reset();
     });
+    
+    $.ajax({
+        url: config.apiUrl + '/p2p/config',
+        type: 'POST',
+        data: JSON.stringify({
+        }),
+        contentType: "application/json",
+        dataType: "json",
+    })
+    .retry(config.retry)
+    .done(function (data) {
+        if(data.success) {
+            //
+            $(document).trigger('haveConfig');
+        }
+        else {
+            msgBoxRedirect(data.error);
+        }
+    })
+    .fail(function (jqXHR, textStatus, errorThrown) {
+        msgBoxNoConn(true); 
+    });
 });
