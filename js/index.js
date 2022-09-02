d1d4ab89813b07f84e3ce52f38117198aa3abae2
vayamos-exchange <admin@vayamos.cc>
2022-09-03 00:02:14 +0200

    Resize columns on mobile

--- js/index.js
+++ js/index.js
@@ -124,13 +124,13 @@ $(document).on('haveConfig', function() {
                     <div class="trade-item row px-2 py-4 hoverable" data-offerid="${v.offerid}">
                         <div class="my-auto sm-w-100 order-1" style="width: 20%">
                             <div class="row">
-	                            <div class="col-4 col-lg-12">
+	                            <div class="col-3 col-lg-12">
 		                            <strong>${v.nickname}</strong>
 		                        </div>
                                 <div class="col-4 col-lg-12">
 		                            <div class="rateit" data-rateit-value="4.5" style="font-size: 20px"></div>
 	                            </div>
-	                            <div class="col-4 col-lg-12">
+	                            <div class="col-5 col-lg-12">
 		                            <span class="small secondary">
 		                                3 transactions | 96%
 		                            </span>
@@ -140,7 +140,7 @@ $(document).on('haveConfig', function() {
                         <div class="my-auto order-2 d-none d-lg-block text-end" style="width: 20%">
                             ${v.price} ${window.p2pInitialFiat}
                         </div>
-                        <div class="my-auto sm-w-50 order-3" style="width: 23%">
+                        <div class="my-auto sm-w-70 order-3" style="width: 23%">
                             <div class="row">
                                 <div class="col-6 d-lg-none secondary">
                                     Available:
@@ -159,7 +159,7 @@ $(document).on('haveConfig', function() {
                         <div class="my-auto sm-w-100 order-5 order-lg-4" style="width: 23%">
                             ${innerPayments}
                         </div>
-                        <div class="my-auto sm-w-50 order-4 order-lg-5" style="width: 14%">
+                        <div class="my-auto sm-w-30 order-4 order-lg-5" style="width: 14%">
                             <button type="button" class="btn ${btnBg} w-100 user-only">${btnText}</button>
                         </div>
                     </div>
