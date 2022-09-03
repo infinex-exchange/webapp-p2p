ed790ddda8a3a9c6e86b70f56c504e3dc525ecc5
vayamos-exchange <admin@vayamos.cc>
2022-09-03 12:24:40 +0200

    h3 -> h5 modal title

--- index.php
+++ index.php
@@ -194,7 +194,7 @@
             <div class="modal-dialog modal-dialog-centered" role="document">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <h3 class="ps-1 modal-title mt-title"></h3>
+                        <h5 class="ps-1 modal-title mt-title"></h5>
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     
