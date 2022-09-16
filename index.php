845007f13e942d9b4d274631c56eb446a9a2fd2a
vayamos-exchange <admin@vayamos.cc>
2022-09-16 23:42:08 +0200

    Rename div

--- index.php
+++ index.php
@@ -148,7 +148,7 @@
                     <h3>Recent P2P trades</h3>
                 </div>
             
-                <div id="recent-trades-data" class="scrollable small d-flex">
+                <div id="transactions-data" class="scrollable small d-flex">
                     <div class="guest-only m-auto">
                         <a class="link-ultra" href="#_" onClick="gotoLogin()">Log In</a> or <a class="link-ultra" href="/account/register">Register</a> to trade
                     </div>
