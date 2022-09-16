6dd93e69cd9452186db7373d943123e4a0582c3a
vayamos-exchange <admin@vayamos.cc>
2022-09-17 00:58:31 +0200

    Update transactions.js

--- js/transactions.js
+++ js/transactions.js
@@ -19,7 +19,7 @@ function renderTransaction(t) {
             </div>
             <div class="col-2">
             </div>
-            <div class="col-6">
+            <div class="col-5">
                 <div class="row">
 		            <div class="col-12 text-end small secondary">
 		                ${cTime}
