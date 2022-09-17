bf0eafdb65b0d395765ea1c5caa804829fdfd483
vayamos-exchange <admin@vayamos.cc>
2022-09-17 10:31:06 +0200

    Padding

--- js/transactions.js
+++ js/transactions.js
@@ -36,7 +36,7 @@ function renderTransaction(t) {
                         </div>
                     </div>
                     
-                    <div class="col-12 pt-1">
+                    <div class="col-12 pt-2">
                         <div class="row">
                             <div class="col-auto me-auto">
                             </div>
