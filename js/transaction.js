cdfdd039d1d7bb53b95857ec6b88e46c7ceb00b9
vayamos-exchange <admin@vayamos.cc>
2022-09-22 11:10:02 +0200

    Fetch transaction from api

--- js/transaction.js
+++ js/transaction.js
@@ -1,58 +1,39 @@
 $(document).ready(function() {
     window.renderingStagesTarget = 1;
-    $(document).trigger('renderingStage');
 });
 
-/*$(document).on('authChecked', function() {
+$(document).on('authChecked', function() {
     if(window.loggedIn) {
-        window.p2pTraAS = new AjaxScroll(
-            $('#transactions-data'),
-            $('#transactions-data-preloader'),
-            {
-                api_key: window.apiKey
-            },
-            function() {
-                this.data.offset = this.offset;
-                var thisAS = this;
-                
-            //---
+        var pathArray = window.location.pathname.split('/');
+        var ptid = pathArray[pathArray.length - 1];
+        
         $.ajax({
-            url: config.apiUrl + '/p2p/transactions',
+            url: config.apiUrl + '/p2p/transaction',
             type: 'POST',
-            data: JSON.stringify(thisAS.data),
+            data: JSON.stringify({
+                api_key: window.api_key
+                ptid: ptid
+            }),
             contentType: "application/json",
             dataType: "json",
         })
         .retry(config.retry)
         .done(function (data) {
             if(data.success) {
-                $.each(data.transactions, function(k, v) {
+                /*$.each(data.transactions, function(k, v) {
                     thisAS.append(renderTransaction(v));
-                });
+                });*/
                 
-                thisAS.done();
-                
-                if(thisAS.data.offset == 0)
-                    $(document).trigger('renderingStage');
-                
-                if(data.transactions.length != 50)
-                    thisAS.noMoreData(); 
+                console.log(data);
+
+                $(document).trigger('renderingStage');
             }
             else {
                 msgBoxRedirect(data.error);
-                thisAS.done();
-                thisAS.noMoreData();
             }
         })
         .fail(function (jqXHR, textStatus, errorThrown) {
             msgBoxNoConn(true);
-            thisAS.done();
-            thisAS.noMoreData();  
         });
-            //---
-            
-            },
-            true
-        );   
     }
-});*/
+});
