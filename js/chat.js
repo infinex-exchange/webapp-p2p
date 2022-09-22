014dee58309765f81bcc43568edc1b9416504dbe
vayamos-exchange <admin@vayamos.cc>
2022-09-22 23:46:45 +0200

    Test login

--- js/chat.js
+++ js/chat.js
@@ -2,5 +2,6 @@ converse.initialize({
 	bosh_service_url: 'https://conversejs.org/http-bind/',
 	view_mode: 'fullscreen',
 	auto_login: true,
-	authentication: 'anonymous'
+	authentication: 'anonymous',
+	jid: 'test@example.com'
 });
