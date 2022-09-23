3822203beab44c3b13f28300bd09050d8c491afd
vayamos-exchange <admin@vayamos.cc>
2022-09-23 13:42:56 +0200

    Styling

--- transaction.php
+++ transaction.php
@@ -57,31 +57,31 @@
         </div>
         
         <div class="row p-2">
-            <div class="col-6 col-lg-2 order-1 order-lg-1 secondary">
+            <div class="col-6 col-lg-2 order-1 order-lg-1 secondary my-auto">
                 Fiat amount:
             </div>
             
-            <div class="col-6 col-lg-2 order-3 order-lg-2 secondary">
+            <div class="col-6 col-lg-2 order-3 order-lg-2 secondary my-auto">
                 Price:
             </div>
             
-            <div class="col-6 col-lg-2 order-5 order-lg-3 secondary">
+            <div class="col-6 col-lg-2 order-5 order-lg-3 secondary my-auto">
                 Crypto amount:
             </div>
             
             <div class="col-6 d-none d-lg-block order-lg-4"></div>
             
-            <div class="col-6 col-lg-2 text-end text-lg-start order-2 order-lg-5">
+            <div class="col-6 col-lg-2 text-end text-lg-start order-2 order-lg-5 my-auto">
                 <strong class="big amount-fiat"></strong>
                 <strong class="big fiatid"></strong>
             </div>
             
-            <div class="col-6 col-lg-2 text-end text-lg-start order-4 order-lg-6">
+            <div class="col-6 col-lg-2 text-end text-lg-start order-4 order-lg-6 my-auto">
                 <span class="price"></span>
                 <span class="fiatid"></span>
             </div>
             
-            <div class="col-6 col-lg-2 text-end text-lg-start order-6 order-lg-7">
+            <div class="col-6 col-lg-2 text-end text-lg-start order-6 order-lg-7 my-auto">
                 <span class="amount-crypto"></span>
                 <span class="assetid"></span>
             </div>
