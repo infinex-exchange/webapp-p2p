98d66be5f0a9262cab6b17b5e70ca6e260c5ba7d
vayamos-exchange <admin@vayamos.cc>
2022-10-01 13:29:17 +0200

    Init selector

--- js/create_offer.js
+++ js/create_offer.js
@@ -2,3 +2,10 @@ $(document).ready(function() {
     window.renderingStagesTarget = 1;
     $(document).trigger('renderingStage');
 });
+
+$(document).on('authChecked', function() {
+    if(!window.loggedIn) return;
+    
+    initCoinSelect('/p2p/assets');
+    initSelectFiat();
+});
