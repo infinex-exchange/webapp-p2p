f7ba46189aa5db1102dec5240b1f51df438f678d
vayamos-exchange <admin@vayamos.cc>
2022-10-01 10:16:28 +0200

    Paddings

--- create_offer.php
+++ create_offer.php
@@ -29,11 +29,11 @@
             </div>
             
             <div class="row pt-1">
-                <div class="col-6 col-lg-3 px-1">
+                <div class="col-6 col-lg-3">
                     <input type="radio" class="btn-check" name="side" id="side-buy" value="BUY" autocomplete="off" checked>
                     <label class="btn w-100 small btn-outline-green px-4" for="side-buy">Buy</label>
                 </div>
-                <div class="col-6 col-lg-3 px-1">
+                <div class="col-6 col-lg-3">
                     <input type="radio" class="btn-check" name="side" id="side-sell" value="SELL" autocomplete="off">
                     <label class="btn w-100 small btn-outline-red px-4" for="side-sell">Sell</label>
                 </div>
