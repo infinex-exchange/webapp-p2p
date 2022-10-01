0b8debedf8747d7e8eb862d08ea735f8d05c1ebf
vayamos-exchange <admin@vayamos.cc>
2022-10-01 10:53:31 +0200

    Layout

--- create_offer.php
+++ create_offer.php
@@ -118,14 +118,14 @@
                 <div class="col-12 ui-card-light p-2">
                     <div class="row">
                         <div class="col-12 text-center">
-                            <strong class="secondary">No payment methods</i>
+                            <strong class="secondary">No payment methods</strong>
                         </div>
                     </div>
                     <div class="row" id="payment-methods-data">
                     </div>
                 </div>
                 
-                <div class="col-12 col-lg-auto">
+                <div class="col-12 col-lg-3">
                     <button type="button" class="btn btn-primary btn-sm w-100">
                         <i class="fa-solid fa-plus"></i>
                         Add
