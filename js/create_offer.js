3dc21e36b4806a86d9efe5ef2fdc67d9a912c4b0
vayamos-exchange <admin@vayamos.cc>
2022-10-01 15:24:57 +0200

    PAdding

--- js/create_offer.js
+++ js/create_offer.js
@@ -69,7 +69,7 @@ $(document).ready(function() {
             
             var innerHtml = $('.select-fpm-item[data-fpmid="' + fpmid + '"]').html();
             $('#payment-methods-data').append(`
-                <div class="col-12 col-md-3 col-lg-3 m-2 hoverable">
+                <div class="col-12 col-md-3 col-lg-3 py-2 mx-2 hoverable">
                     <div class="row">
                         ${innerHtml}
                     </div>
