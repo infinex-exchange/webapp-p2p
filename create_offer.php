9567d709fa119217915d65ccfd0ad0a2be75f0b2
vayamos-exchange <admin@vayamos.cc>
2022-10-01 10:30:04 +0200

    Layout

--- create_offer.php
+++ create_offer.php
@@ -20,12 +20,41 @@
         <!-- Main column -->
         <div class="col-12 col-lg-8 p-0 ui-card ui-column">
             
-            <div class="row">
+            <div class="row pb-3">
                 <h3>Create offer</h3>
             </div>
             
-            <div class="row pt-3">
-                <h5 class="secondary">Side:</h5>
+            <div class="row">
+                <div class="col-3 col-lg-1 mt-auto pb-2 px-1">
+                    <input type="radio" class="btn-check" name="side" id="side-buy" value="BUY" autocomplete="off" checked>
+                    <label class="btn w-100 small btn-outline-green px-4" for="side-buy">Buy</label>
+                </div>
+                <div class="col-3 col-lg-1 mt-auto pb-2 px-1">
+                    <input type="radio" class="btn-check" name="side" id="side-sell" value="SELL" autocomplete="off">
+                    <label class="btn w-100 small btn-outline-red px-4" for="side-sell">Sell</label>
+                </div>
+                
+                <div class="col-6 col-lg-3 my-auto pb-2 px-1">
+                    <div class="row">
+                        <div class="col-12 pb-1">
+                            <h6 class="secondary">Coin:</h6>
+                        </div>
+                        <div class="col-12">
+                            <?php include(__DIR__.'/../../templates/select_coin.php'); ?>
+                        </div>
+                    </div>
+                </div>
+                
+                <div class="col-6 col-lg-3 my-auto pb-2 px-1">
+                    <div class="row">
+                        <div class="col-12 pb-1">
+                            <h6 class="secondary">Fiat:</h6>
+                        </div>
+                        <div class="col-12">
+                            <?php include(__DIR__.'/templates/select_fiat.php'); ?>
+                        </div>
+                    </div>
+                </div>
             </div>
             
             <div class="row pt-1">
