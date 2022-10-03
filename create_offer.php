f3725829ea90845193071a7fee70538e28a5965c
vayamos-exchange <admin@vayamos.cc>
2022-10-03 12:50:32 +0200

    Layout

--- create_offer.php
+++ create_offer.php
@@ -30,11 +30,11 @@
             </div>
             
             <div class="row mb-5">
-                <div class="col-6 col-lg-2 mt-auto px-1">
+                <div class="col-6 col-lg-2 mt-auto">
                     <input type="radio" class="btn-check" name="side" id="side-buy" value="BUY" autocomplete="off" checked>
                     <label class="btn w-100 small btn-outline-green px-4" for="side-buy">Buy</label>
                 </div>
-                <div class="col-6 col-lg-2 mt-auto px-1">
+                <div class="col-6 col-lg-2 mt-auto">
                     <input type="radio" class="btn-check" name="side" id="side-sell" value="SELL" autocomplete="off">
                     <label class="btn w-100 small btn-outline-red px-4" for="side-sell">Sell</label>
                 </div>
