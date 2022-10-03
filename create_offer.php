23bc0b8ca26da873a1b76827b5e2cf6163009cd8
vayamos-exchange <admin@vayamos.cc>
2022-10-03 13:00:10 +0200

    Mobile layout

--- create_offer.php
+++ create_offer.php
@@ -30,16 +30,16 @@
             </div>
             
             <div class="row mb-5">
-                <div class="col-6 col-lg-2 mt-auto pe-1">
+                <div class="col-6 col-lg-2 mb-5 mb-lg-0 mt-auto pe-1">
                     <input type="radio" class="btn-check" name="side" id="side-buy" value="BUY" autocomplete="off" checked>
                     <label class="btn w-100 small btn-outline-green px-4" for="side-buy">Buy</label>
                 </div>
-                <div class="col-6 col-lg-2 mt-auto ps-1">
+                <div class="col-6 col-lg-2 mb-5 mb-lg-0 mt-auto ps-1">
                     <input type="radio" class="btn-check" name="side" id="side-sell" value="SELL" autocomplete="off">
                     <label class="btn w-100 small btn-outline-red px-4" for="side-sell">Sell</label>
                 </div>
                 
-                <div class="col-12 col-lg-4 my-auto px-lg-1">
+                <div class="col-12 col-lg-4 my-auto my-5 my-lg-0 px-lg-1">
                     <div class="row">
                         <div class="col-12 pb-1">
                             <h5 class="secondary">Coin:</h5>
