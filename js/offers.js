58a309d1c08c40c0f1c1a9714632530b962b7b5c
vayamos-exchange <admin@vayamos.cc>
2022-10-05 16:07:25 +0200

    Add user icon

--- js/offers.js
+++ js/offers.js
@@ -172,7 +172,8 @@ $(document).on('haveConfig', function() {
                         <div class="my-auto sm-w-100 order-1" style="width: 20%">
                             <div class="row">
 	                            <div class="col-3 col-lg-12">
-		                            <strong>${v.nickname}</strong>
+		                            <i class="fa-solid fa-user"></i>
+                                    <strong>${v.nickname}</strong>
 		                        </div>
                                 <div class="col-4 col-lg-12">
 		                            <div class="rateit" data-rateit-value="${ratingStars}" style="font-size: 20px"></div>
