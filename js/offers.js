654f4a18cbcb7fceb3ea63980165e131643781c1
vayamos-exchange <admin@vayamos.cc>
2022-10-05 16:08:52 +0200

    Replace icon

--- js/offers.js
+++ js/offers.js
@@ -172,7 +172,7 @@ $(document).on('haveConfig', function() {
                         <div class="my-auto sm-w-100 order-1" style="width: 20%">
                             <div class="row">
 	                            <div class="col-3 col-lg-12">
-		                            <i class="fa-regular fa-circle-user"></i>
+		                            <i class="fa-solid fa-circle-user"></i>
                                     <strong>${v.nickname}</strong>
 		                        </div>
                                 <div class="col-4 col-lg-12">
