a9301564bdcdce09bdaf3d200e0eea276a286d9d
vayamos-exchange <admin@vayamos.cc>
2022-10-07 22:41:20 +0200

    No wrap

--- js/offers.js
+++ js/offers.js
@@ -171,8 +171,9 @@ $(document).on('haveConfig', function() {
                      data-fiat-min="${v.fiat_min}" data-fiat-max="${v.fiat_max}" data-price="${v.price}">
                         <div class="my-auto sm-w-100 order-1" style="width: 20%">
                             <div class="row">
-	                            <div class="col-4 col-lg-12">
-		                            <i class="fa-solid fa-circle-user"></i>&nbsp;<strong>${v.nickname}</strong>
+	                            <div class="col-4 col-lg-12 text-nowrap">
+		                            <i class="fa-solid fa-circle-user"></i>
+		                            <strong>${v.nickname}</strong>
 		                        </div>
                                 <div class="col-4 col-lg-12">
 		                            <div class="rateit" data-rateit-value="${ratingStars}" style="font-size: 20px"></div>
