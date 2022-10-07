2b9f080dd7e1fdcdb7eb3d78312cca0df3b446cf
vayamos-exchange <admin@vayamos.cc>
2022-10-07 22:55:03 +0200

    Padding

--- js/offers.js
+++ js/offers.js
@@ -173,7 +173,7 @@ $(document).on('haveConfig', function() {
                             <div class="row">
 	                            <div class="col-4 col-lg-12">
 	                                <div class="row flex-nowrap">
-		                                <div class="col-auto my-auto d-none d-lg-block pe-p">
+		                                <div class="col-auto my-auto d-none d-lg-block pe-0">
 				                            <i class="fa-solid fa-circle-user"></i>
 				                        </div>
 				                        <div class="col-auto my-auto text-break">
